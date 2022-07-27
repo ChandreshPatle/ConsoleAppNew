0899feecffdc80e8609a6358872fdaaeb05039c9
Harish <patle.chandresh@gmail.com>
2022-07-27 19:01:19 +0530

    Add project files.

---
+++ ConsoleAppNew/MainApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNew
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main started");
+            /*
+            Student student = new Student();//object creation
+            student.DisplayStudentDetails();
+            student.SetStudentDetails(101, "Chandresh", 30);
+            student.DisplayStudentDetails(); */
+            /*
+            var student = new Student();
+            student.SetStudentDetails(2000, "Aniket", 25);
+            student.DisplayStudentDetails();//2000,Aniket,25
+
+            new Student().SetStudentDetails(3000, "Tejas", 24);
+            new Student().DisplayStudentDetails();//0,null,0
+            */
+            //Method calling
+            //ref & out
+            MethodCalling methodCalling = new MethodCalling();
+            int a = 20, b = 30;
+            //Console.WriteLine($"a:{a}\tb:{b}");//20,30
+
+            //methodCalling.CallByValue(a, b);//from method:50, 50
+            //methodCalling.CallByRef(ref a,ref b);
+
+            // Console.WriteLine($"a:{a}\tb:{b}");//20,30
+            //Console.WriteLine($"a:{a}\tb:{b}");//50,30
+
+            //int sum, multiply;
+            //methodCalling.CallByOut(a, b, out sum, out multiply);
+            //Console.WriteLine($"Sum:{sum}\tMultiplication:{multiply}");//50,600
+            /*
+            int[] arr = {1,2,3,4};
+            int result = methodCalling.CallByVarArgs("astric",arr);
+            Console.WriteLine($"Sum is:{result}");
+            Console.WriteLine($"Sum is:{methodCalling.CallByVarArgs("astric",20,30)}");
+            Console.WriteLine($"Sum is:{methodCalling.CallByVarArgs("astric", 20,30,50)}");
+            Console.WriteLine($"Sum is:{methodCalling.CallByVarArgs("astric", 20,30,50,60,70)}");
+            */
+            methodCalling.Message("Narendra","Patil",20000);
+
+
+
+        }
+    }
+}
+
---
+++ ConsoleAppNew/Program.cs
@@ -0,0 +1,156 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleAppNew
+//{
+//  //  internal class Program
+//  //{
+//  // object id;  //valid
+//  //dynamic mark; //valid
+//  //var //invalid
+
+///* static void Main(string[] args)
+// { /*
+//     //Console.WriteLine("Welcome to C#");
+//     Console.WriteLine("This is NextLine");
+//     Console.WriteLine();
+//     for (int i = 0; i < 10; i++)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     Console.WriteLine("Mains Ends");*/
+
+////using Read() method
+///*
+//int charUnicode;
+//Console.WriteLine("Enter any character:");
+
+//charUnicode=Console.Read();
+//Console.WriteLine("Char unicode is:"+charUnicode);
+//*/
+///*
+//int id;
+//string name;
+//float salary;
+//char grade;
+
+//Console.WriteLine("Input details:");
+
+//Console.WriteLine("Enter ID:");
+//id = Int32.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter Name:");
+//name = Console.ReadLine();
+
+//Console.WriteLine("Enter Salary:");
+//salary = Single.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter Grade:");
+//grade = Char.Parse(Console.ReadLine());
+//*/
+//// Int32.Parse(string);
+//// Single.Parse(string);
+////Char.parse(string)
+
+////Console.WriteLine("Details are:");
+///*
+//Console.Write("\tID:" + id);
+//Console.Write("\tName:"+ name);
+//Console.Write("\tSalary:" + salary);
+//Console.WriteLine("\tGrade:" + grade);
+//*/
+///*
+//Console.WriteLine("ID:"+id+"\tName:"+name+"\tSalary:"+salary+"\tGrade:"+grade);
+//Console.WriteLine("ID:{0}\tName:{1}\tSalary:{2}\tGrade:{3}", id,name,salary, grade);
+//Console.WriteLine("ID:{id}\tName:{name}\tSalary:{salary}\tGrade:{grade}");
+//*/
+
+////using ReadKey() => returns ConsoleKeyInfo(its a predefined structure)
+///*
+//            Console.WriteLine("Press any key:");
+//            //ConsoleKeyInfo ck = Console.ReadKey();
+//            ConsoleKeyInfo ck = Console.ReadKey(true);
+//            Console.Write("*");
+
+//            //ConsoleKey
+//            Console.WriteLine("\nPressed Key:"+ ck.Key);
+//            Console.WriteLine("Pressed KeyChar:"+ ck.KeyChar);
+//*/
+
+///*
+//            Console.Write("Enter Password :");
+//            string pwd = "";
+
+//            ConsoleKeyInfo ck = Console.ReadKey(true);
+
+//            while (ck.Key != ConsoleKey.Enter)
+//            {
+//                Console.Write("*");
+//                pwd = pwd + ck.KeyChar;
+//                ck = Console.ReadKey(true);
+//            }
+//            Console.Write($"\nPassword is:{pwd}");
+//*/
+
+////Boxing-> conversion from value type to reference type
+
+///*int x = 20;
+//object obj = x;//boxing, implicit
+//Console.WriteLine($"value:{obj}\t type:{obj.GetType()}");//20, object
+
+////UnBoxing
+////
+//int y = (int)obj;
+//Console.WriteLine($"value:{y}\t type:{y.GetType()}");//20, int
+
+//float z =(int)obj;
+//Console.WriteLine($"value:{z}\t type:{z.GetType()}");//20,Single
+
+//var obj1 = x;
+//int a = obj1;
+//Console.WriteLine($"value:{obj1}\t type:{obj1.GetType()}");//20, int
+
+//var obj2 = new StringBuilder("string data");
+
+//dynamic d = x;
+//int b = d;
+//Console.WriteLine($"value:{d}\t type:{d.GetType()}");//20, int*/
+///*
+//int choice = 20;
+
+//switch (choice)
+//{
+//    case 1:
+//        //Console.WriteLine("Once");
+//        //break;
+//    case 2:
+//        Console.WriteLine("simgle digit");
+//        break;
+
+//    default:
+//        Console.WriteLine("invalid");
+//        break;
+//}
+
+////foreach loop
+//int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+//Console.WriteLine("Array data:");
+//foreach(var item in arr)
+//{
+//    Console.WriteLine(item);    
+//}
+///* foreach(type identifier in collection-name){
+// * statement
+// * 
+// * its forward only loop
+// * its readonly 
+// * }
+
+
+//}
+//}
+//} */
