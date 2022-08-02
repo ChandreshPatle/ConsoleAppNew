ffde3a9d5cd782694f5b062ee7b2b4674058cd04
Harish <patle.chandresh@gmail.com>
2022-08-02 12:43:45 +0530

    Day8

---
+++ ConsoleAppNew/Day8/IndexerMain.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleAppNew.Day8
+//{
+//    class Employee
+//    {
+//        long[] _ContactNo;
+//        public Employee()
+//        {
+//            _ContactNo = new long[5]; 
+//        }
+//        //Indexer
+//        public long this[int index]
+//        {
+//            get { return _ContactNo[index]; }
+//            set { _ContactNo[index] = value; }
+//        }
+//        /*
+//        //INdexer Overloading
+//        public long this[int row,int col]
+//        {
+//            get { return _ContactNo[index]; }
+//            set { _ContactNo[index] = value; }
+//        }
+//        */
+
+//        //Readonly property to get ContactNo length
+//        public long ContactNoLength
+//        {
+//            get => _ContactNo.Length;
+//        }
+//    }
+//    internal class IndexerMain
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee employee = new Employee();
+//            employee[0] = 8600409685;//write operaation,set accessor
+//            long contactNo = employee[0];//read operation, get accessor
+//            Console.WriteLine($"{contactNo}");
+//            //string str = "gjghfj";
+//            //Console.WriteLine(str[3]);
+
+//            for (int i = 0; i < employee.ContactNoLength; i++)
+//            {
+//                Console.WriteLine($"Input contact no {i+1}:");
+//                employee[i] = Int64.Parse(Console.ReadLine());
+//            }
+//            Console.WriteLine("Employee Contact details are:");
+//            for (int i = 0; i < employee.ContactNoLength; i++)
+//            {
+//                Console.WriteLine(employee[i]);
+//            }
+
+
+
+
+//        }
+//    }
+//}
---
+++ ConsoleAppNew/Day8/StringDemo.cs
@@ -0,0 +1,77 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Text.RegularExpressions;
+
+//namespace ConsoleAppNew.Day8
+//{
+//    internal class StringDemo
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string str = "abcd123efgh456ijk789lmn110pq556rstu76vwxy6655z";
+//            //123,456,
+//            //For regular Expression
+//            /*
+//                Regex
+//                Match
+//                MatchCollection
+//                */
+//            bool result = Regex.IsMatch(str, "\\d");
+//            Console.WriteLine(result);
+            
+//            result = Regex.IsMatch(str, @"\d{2}");
+//            Console.WriteLine(result);
+//            Console.WriteLine(str);
+//            Console.WriteLine("=======================");
+
+//            if (result)
+//            {
+//                /*
+//                //Match match = Regex.Match(str, @"\d{2}");
+//                Match match = Regex.Match(str, @"\d{2,}");
+//                Console.WriteLine(match.Success);
+//                Console.WriteLine(match.Name);
+//                Console.WriteLine(match.Value);
+//                Console.WriteLine(match.Length);
+
+//                match.NextMatch();
+//                Console.WriteLine("=======================");
+//                Console.WriteLine(match.Name);
+//                Console.WriteLine(match.Value);
+//                Console.WriteLine(match.Length);
+//                */
+
+//                MatchCollection matches =Regex.Matches(str, @"[0-9]{2,}");
+//                foreach (Match match in matches)
+//                {
+//                    Console.WriteLine(match.Value);
+//                }
+//                Console.WriteLine("=======================");
+
+//                string[] data = Regex.Split(str, @"\d{2,}");
+//                foreach (var item in data)
+//                {
+//                    Console.WriteLine(item);
+//                }
+//                Console.WriteLine("=======================");
+//                string newdata = Regex.Replace(str, @"\d{2,}","@");
+//                Console.WriteLine(newdata);
+
+
+//                //Random class is used to generate random values
+//                Random random = new Random();
+//                Console.WriteLine("=======================");
+//                for (int i = 0; i < 10; i++)
+//                {
+//                    int num = random.Next(100000, 999999);
+                                
+//                    Console.WriteLine(num);
+//                }
+                
+//            }
+//        }
+//    }
+//}
